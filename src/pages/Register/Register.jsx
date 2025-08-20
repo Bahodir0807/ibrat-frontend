19de6527bcb89b9ace89a147a50cc4226fa86d77
Bahodir0807 <sultonovbahodir00@gmail.com>
2025-08-20 19:00:38 +0500

    a

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function LogRegister() {
+export default function Register() {
   const [form, setForm] = useState({
     username: "",
     password: "",
